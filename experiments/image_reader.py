704e0c3534eb37c4d06aeb70d44f2028570654f9
Pankaj Bhattarai <pankajbhattarai100@gmail.com>
2019-11-15 16:21:08 +0545

    add display image function

---
+++ experiments/image_reader.py
@@ -0,0 +1,9 @@
+from PIL import Image
+
+
+def display_image(path: str):
+    img = Image.open(path)
+    img.show()
+
+
+display_image('/home/vipero_7/PycharmProjects/face_recognition/data/dataset/test/0000_02176.pgm')
