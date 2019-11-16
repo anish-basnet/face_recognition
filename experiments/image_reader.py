16c066f23cfc226d3a26801c025b1901f756dd89
Anish <anishbasnetworld@gmail.com>
2019-11-16 14:25:48 +0545

    modified display image

--- experiments/image_reader.py
+++ experiments/image_reader.py
@@ -5,5 +5,4 @@ def display_image(path: str):
     img = Image.open(path)
     img.show()
 
-
-display_image('/home/vipero_7/PycharmProjects/face_recognition/data/dataset/test/0000_02176.pgm')
+# display_image('/home/vipero_7/PycharmProjects/face_recognition/data/dataset/test/0000_02176.pgm')
