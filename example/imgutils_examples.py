1508bdc65cc6621fa0d49eb813581ce1e2d70b0a
Anish <anishbasnetworld@gmail.com>
2019-11-16 14:15:15 +0545

    Refactor code

--- example/imgutils_examples.py
+++ example/imgutils_examples.py
@@ -1,5 +1,18 @@
+"""
+author : Anish Basnet, Pankaj Bhattrai
+Email : anishbasnetworld@gmail.com
+"""
+
+
 import os
-from face_recognition.utils.imgutils import display_image
+
+import cv2
+import numpy as np
+
+from face_recognition.utils.imgutils import return_image
+
 if __name__ == '__main__':
     path = os.path.join('data', 'dataset', 'test', '0000_02176.pgm')
-    display_image(path)
+    image = cv2.cvtColor(np.array(return_image(path)), cv2.COLOR_RGB2BGR)
+    cv2.imshow('image', image)
+    cv2.waitKey(0)
--- face_recognition/utils/imgutils.py
+++ face_recognition/utils/imgutils.py
@@ -1,7 +1,17 @@
-from PIL import Image
+"""
+author : Anish Basnet, Pankaj Bhattrai
+Email : anishbasnetworld@gmail.com
+"""
+
+from PIL import ImageFile, Image
 
 
-def display_image(path):
-    img = Image.open(path)
-    img.show()
-
+def return_image(file_path):
+    """
+    This function convert the image into rgb format.
+    :param file_path: str -> path of an image
+    :return: image -> return image
+    """
+    ImageFile.LOAD_TRUNCATED_IMAGES = True
+    image = Image.open(file_path).convert('RGB')
+    return image
