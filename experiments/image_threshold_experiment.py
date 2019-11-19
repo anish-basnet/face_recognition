54e46d57537db9c849938ff07bd2179ca900b655
Anish <anishbasnetworld@gmail.com>
2019-11-20 01:21:56 +0545

    Refactor image threshold

--- experiments/image_threshold_experiment.py
+++ experiments/image_threshold_experiment.py
@@ -6,7 +6,7 @@ import os
 import pickle
 import cv2
 import numpy as np
-from collections import
+from collections import OrderedDict
 from face_recognition.utils.imgutils import return_image
 
 if __name__ == '__main__':
@@ -14,13 +14,12 @@ if __name__ == '__main__':
     files = os.listdir(path)
     write_path = os.path.join('data', 'processing', 'threshold', 'test.pkl')
 
-    vectors =
+    vectors = OrderedDict()
     for file in files:
         bgr_image = cv2.cvtColor(np.array(return_image(file)), cv2.COLOR_RGB2BGR)
         gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
         threshold_image = cv2.adaptiveThreshold(gray_image, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 20)
         vectors[file] = np.array(threshold_image)
-        # cv2.waitKey(0)
 
     with open(write_path, 'wb') as fid:
         pickle.dump(vectors, fid)
