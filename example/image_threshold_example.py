a341140b5a50ea470bc1b510f6253ee23da6f014
vipero7 <pankajbhattarai100@gmail.com>
2019-11-22 14:22:27 +0545

    thresholding

--- example/image_threshold_example.py
+++ example/image_threshold_example.py
@@ -4,7 +4,7 @@ Email : pankajbhattarai100@gmail.com
 """
 import os
 import pickle
-from collections import orderddict
+from collections import OrderedDict
 
 import cv2
 import numpy as np
@@ -16,7 +16,7 @@ if __name__ == '__main__':
     files = os.listdir(path)
     write_path = os.path.join('data', 'processing', 'threshold', 'test.pkl')
 
-    vectors = orderddict
+    vectors = OrderedDict
     for file in files:
         bgr_image = cv2.cvtColor(np.array(return_image(file)), cv2.COLOR_RGB2BGR)
         gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
--- experiments/image_threshold_experiment.py
+++ experiments/image_threshold_experiment.py
@@ -6,7 +6,7 @@ import os
 import pickle
 import cv2
 import numpy as np
-from collections import
+from collections import OrderedDict
 from face_recognition.utils.imgutils import return_image
 
 if __name__ == '__main__':
@@ -14,7 +14,7 @@ if __name__ == '__main__':
     files = os.listdir(path)
     write_path = os.path.join('data', 'processing', 'threshold', 'test.pkl')
 
-    vectors =
+    vectors = OrderedDict
     for file in files:
         bgr_image = cv2.cvtColor(np.array(return_image(file)), cv2.COLOR_RGB2BGR)
         gray_image = cv2.cvtColor(bgr_image, cv2.COLOR_BGR2GRAY)
